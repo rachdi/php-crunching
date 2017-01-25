10caaf930ab82e05e0d7b90f0b48e550bc290da6
rachdi <bigking3100@gmail.com>
2017-01-25 10:00:15 +0100

    nbre de mot font exactement 15 caractères

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php 
 $string=file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico= explode("\n",$string);
-str_word_count($string);
-echo "ce dictionnaire contient ".str_word_count($string)." mot"; 
 
+$nombremot=str_word_count($string);
+echo "ce dictionnaire contient ".$nombremot." mot"; 
+function nbrecarac($dico){
+	$i=0;
+	foreach ($dico as $key => $value) {
 
+		if (strlen($value)===15) {
+			$i++;
+		};
+	}
+	echo "il y'as ".strval($i)." font exactement 15 caractères";
+}
+nbrecarac($dico);
 ?>
